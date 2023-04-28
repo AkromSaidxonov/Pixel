c1576055c370ca919e6537705b5149a04aaafcf6
AkromSaidxonov <101202313+AkromSaidxonov@users.noreply.github.com>
2023-04-28 15:24:17 +0500

    28042023

--- src/common/store/apiSlice/apiSlice.js
+++ src/common/store/apiSlice/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://pixel.up.railway.app/api",
+  baseUrl: "http://95.130.227.78:8080",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
